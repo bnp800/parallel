af9319e772a332159f98b4ee75f1fe841e225369
bnp800 <bnp800onsteam@gmail.com>
2020-11-18 21:14:40 +0300

    pthread time

--- test3/prime_p.cpp
+++ test3/prime_p.cpp
@@ -3,31 +3,33 @@
 #include<cstdlib>
 #include<fstream>
 #include<math.h>
-
+#include<time.h>
 using namespace std;
 
 bool *is_prime;
 int* data;
-
+double totaltime = 0,maxtime = 0;
 struct thread_data
 {
-	int start;
-	int end;
-	int num;
+    int start;
+    int end;
+    int num;
 };
 void* primes(void *ptr)
 {
-	struct thread_data *my_data;
-	my_data = (struct thread_data*)ptr;
-	int newbegin = my_data->start + (my_data->num - my_data->start % my_data->num) % my_data->num;
-	if(newbegin > my_data->end)
-	    pthread_exit(NULL);
-	for(int i = newbegin;i < my_data->end;i += my_data->num)
-	    is_prime[i] = false;
+    double begintime,endtime;
+    struct thread_data *my_data;
+    my_data = (struct thread_data*)ptr;
+    int newbegin = my_data->start + (my_data->num - my_data->start % my_data->num) % my_data->num;
+    if(newbegin > my_data->end)
+	pthread_exit(NULL);
+    for(int i = newbegin;i < my_data->end;i += my_data->num)
+	is_prime[i] = false;
 }
 int main(int argc,char* argv[])
 {
     int num = atoi(argv[1]),begin = atoi(argv[2]),end = atoi(argv[3]);
+    clock_t begintime,endtime;
     struct thread_data td[num];
     is_prime = new bool [end-begin];
     data = new int [end-begin];
@@ -41,20 +43,27 @@ int main(int argc,char* argv[])
 	{
 	    for(int k = j + j;k <= partion_size;k+=j)
 		is_prime[k] = false;
-	for(int i = 1;i < num;i++)
-	{
-	    td[i].num = j;
-	    td[i].start = i * partion_size;
-	    td[i].end = (i+1) * partion_size;
-	    if(td[i].start < 2)
-		td[i].start = 2;
-	    if(td[i].end > end + 1)
-		td[i].end = end + 1;
-	    int ret = pthread_create(&tids[i],NULL,primes,(void*) &td[i]);
-	    if(ret)
+	    for(int i = 1;i < num;i++)
 	    {
-		cerr << "Error create thread:" << ret << endl;
-		exit(-1);
+		td[i].num = j;
+		td[i].start = i * partion_size;
+		td[i].end = (i+1) * partion_size;
+		if(td[i].start < 2)
+		    td[i].start = 2;
+		if(td[i].end > end + 1)
+		    td[i].end = end + 1;
+    begintime = clock();
+		int ret = pthread_create(&tids[i],NULL,primes,(void*) &td[i]);
+    endtime = clock();
+    double mytime = (double)(endtime - begintime) / CLOCKS_PER_SEC;
+    totaltime += mytime;
+    if(mytime > maxtime)
+	maxtime = mytime;
+		if(ret)
+		{
+		    cerr << "Error create thread:" << ret << endl;
+		    exit(-1);
+		}
 	    }
 	    for(int l = 1;l < num;l++){
 		int rc = pthread_join(tids[l],NULL);
@@ -65,14 +74,26 @@ int main(int argc,char* argv[])
 		}
 	    }
 	}
+    }
+
+    int count = 0;
+    ofstream out,time;
+    out.open("primes.txt",ios::out);
+    time.open("pthread_time.dat",ios::out | ios::app);
+    for(int i = begin;i < end;i++)
+    {
+	if(is_prime[i])
+	{
+	    count++;
+	    out << i << " ";
 	}
     }
-    for(int i =2;i < end;i++)
-	if(is_prime[i])
-	    cout << i << " ";
-    cout << endl;
+    time << num << " " << totaltime << " " << maxtime << endl;
+    cout << "Total found " << count << " prime numbers" << endl;
+    out.close();
+    time.close();
     delete [] is_prime;
     delete [] data;
     pthread_exit(NULL);
-   // return 0;
+    // return 0;
 }
