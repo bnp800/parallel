e8bae0de015b5adb491279b3c7b050ea2380981f
bnp800 <bnp800onsteam@gmail.com>
2020-12-17 00:52:36 +0300

    update test 3 17/12/20

--- test3/prime.c
+++ test3/prime.c
@@ -7,149 +7,142 @@
 #include <stdbool.h>
 #include <fcntl.h>
 
-bool* is_prime;
-int* data;
+
 
 void Help(char* prog_name);
 int main(int argc, char* argv[])
 
 {
-  int n, l, mycount = 0, totalcount = 0, myrank, task_num, partion_size, begin,end;
+	int  mycount = 0, totalcount = 0, myrank, task_num, partion_size, begin,end,begin_g =
+		1,end_g = 1;
 
-  double start_time, end_time, total_time = 0, my_time = 0;
+	double start_time, end_time, total_time = 0, my_time = 0;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &task_num);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &task_num);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	if (argc != 4)
+	{
+		Help(argv[0]);
+		exit(0);
+	}
 
-  if (argc != 4)
-  {
-    Help(argv[0]);
-    exit(0);
-  }
 
-  if (myrank == 0)
-  {
-    l = atoi(argv[1]);
-    n = atoi(argv[2]);
-    is_prime = (bool*)malloc(n+1 * sizeof(bool));
-    data = (int*)malloc(n+1 * sizeof(int));
-  }
-  MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&l, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	begin_g = atoi(argv[1]);
+	end_g = atoi(argv[2]);
 
-  if (myrank != 0) 
-  {
-    is_prime = (bool*)malloc(n * sizeof(bool));
-    data = (int*)malloc(n * sizeof(int));
-  }
+	partion_size = (int)floor((end_g-begin_g) * 1.0 / task_num);
+	begin = (myrank) * partion_size + begin_g;
+	end = (myrank + 1) * partion_size + begin_g;
+	if(myrank == task_num - 1)
+		end = end_g + 1;
+	int length = end - begin;
+	int offset[task_num],count[task_num];
+	int i,k;
+	MPI_Gather(&length,1,MPI_INT,count,1,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Bcast(count,task_num,MPI_INT,0,MPI_COMM_WORLD);
+	if(myrank == 0)
+	{
+		for(i = 0;i < task_num;i++)
+			for(offset[0] = 0,i = 1;i < task_num;offset[i] = offset[i-1] + count[i-1],i++);
+	}
+	MPI_Bcast(offset,task_num,MPI_INT,0,MPI_COMM_WORLD);
+	bool global_prime[end_g-begin_g];
+	bool local_prime[length];
+	bool is_prime[end_g];
+	for(int i = 3;i < end_g;i++)
+	{
+		if((i % 2) == 0)
+			is_prime[i] = false;
+		else 
+			is_prime[i] = true;
+		is_prime[2] = true;
+	}
+	for(int i = begin_g;i < end_g;i++)
+		global_prime[i] = is_prime[i];
+	MPI_Bcast(global_prime,end_g-begin_g,MPI_C_BOOL,0,MPI_COMM_WORLD);
+	MPI_Bcast(is_prime,end_g,MPI_C_BOOL,0,MPI_COMM_WORLD);
+	//printf("Process %d: length:%d offset:%d begin:%d end:%d\n",myrank,count[myrank],offset[myrank],begin,end);
+	MPI_Scatterv(global_prime,count,offset,MPI_C_BOOL,local_prime,count[myrank],MPI_C_BOOL,0,MPI_COMM_WORLD);
+	start_time = MPI_Wtime();
+	if (myrank == 0)
+	{
+		int j;
+		for (i = 2; i * i < end_g; i++)
+		{
+			if (is_prime[i])
+			{
+				for (j = 2 * i; j < end; j = j + i)
+					local_prime[j-begin] = false;
 
-  MPI_Bcast(is_prime,n , MPI_C_BOOL, 0, MPI_COMM_WORLD);
-  MPI_Bcast(data, n , MPI_INT, 0, MPI_COMM_WORLD);
+				MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			}
+		}
+		i = 0;
+		printf("FIN\n");
+		MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	}
 
-  start_time = MPI_Wtime();
-  partion_size = (int)ceil(n * 1.0 / task_num);
-  begin = myrank * partion_size;
-  end = (myrank + 1) * partion_size;
+	else
+	{
+		int new_begin;
+		while (1)
+		{
+			MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  if (begin < 2)
-    begin = 2;
-  if (end > n + 1)
-    end = n + 1;
+			if (i == 0)
+				break;
 
-  //printf("%d %d %d\n",begin,end,myrank);
-  int i, k;
-  k = 0;
+			new_begin = begin + (i - begin % i) % i;
 
-  for (i = 0; i < end; i++)
-    is_prime[i] = true;
-  if (myrank == 0)
-  {
-    int j;
-    for (i = 2; /*i < partion_size && */i * i < n; i++)
-    {
-      if (is_prime[i])
-      {
-        for (j = i + i; j < partion_size; j = j + i)
-          is_prime[j] = false;
+			if (new_begin > end)
+				break;
 
-        data[k] = i;
-        MPI_Bcast(data + k, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        k++;
-      }
-    }
-    data[k] = 0;
+			for (k = new_begin; k < end; k += i)
+				if(k != i)
+					local_prime[k-begin] = false;
 
-    MPI_Bcast(data + k, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  }
+		}
+	}
+	MPI_Gatherv(local_prime,count[myrank],MPI_C_BOOL,global_prime,count,offset,MPI_C_BOOL,0,MPI_COMM_WORLD);
+	end_time = MPI_Wtime();
+	double max_time = 0;
+	my_time = end_time - start_time;
 
-  else
-  {
-    int new_begin;
-    while (1)
-    {
-      MPI_Bcast(data + k, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&my_time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&my_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-      if (data[k] == 0)
-        break;
+	FILE* fp = NULL;
+	FILE* time_stats = NULL;
 
-      new_begin = begin + (data[k] - begin % data[k]) % data[k];
+	fp = fopen(argv[3], "a");
+	time_stats = fopen("time.dat", "a+");
 
-      if (new_begin > end)
-        break;
+	if (myrank == 0)
+	{
+		for(int i = 0;i < end_g - begin_g;i++)
+		{
+			if(global_prime[i])
+			{
+				totalcount++;
+				fprintf(fp,"%d ",i + begin_g);
+				//	printf("%d ",i+begin_g);
+			}
+		}
+		printf("%d\n", totalcount);
+		fprintf(time_stats, "%d %f %f\n", task_num, total_time, max_time);
+	}
 
-      for (i = new_begin; i < end; i += data[k])
-	  if(i != data[k])
-        is_prime[i] = false;
-
-      k++;
-    }
-  }
-
-  end_time = MPI_Wtime();
-  double max_time = 0;
-  my_time = end_time - start_time;
-
-  MPI_Reduce(&my_time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  MPI_Reduce(&my_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-
-  FILE* fp = NULL;
-  FILE* time_stats = NULL;
-
-  fp = fopen(argv[3], "a");
-  time_stats = fopen("time.dat", "a+");
-
-  if (begin < l)
-    begin = l;
-  for (i = begin; i < end; i++)
-    if (is_prime[i])
-    {
-      fprintf(fp, "%d ", i);
-      mycount++;
-    }
-
-  MPI_Reduce(&mycount, &totalcount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-  if (myrank == 0)
-  {
-    printf("%d\n", totalcount);
-    fprintf(time_stats, "%d %f %f\n", task_num, total_time, max_time);
-  }
-
-  fclose(fp);
-  fclose(time_stats);
-  free(data);
-  free(is_prime);
-  MPI_Finalize();
-
-  if (myrank != 0)
-    exit(0);
-  else
-    return 0;
+	fclose(fp);
+	fclose(time_stats);
+	//	free(data);
+	//	free(is_prime);
+	MPI_Finalize();
+	exit(0);
 }
 
 void Help(char* prog_name)
 
 {
-  fprintf(stderr, "Please input the command as follow: %s <n> <m> <filename>\n", prog_name);
+	fprintf(stderr, "Please input the command as follow: %s <n> <m> <filename>\n", prog_name);
 }
--- test3/prime_p.cpp
+++ test3/prime_p.cpp
@@ -7,109 +7,116 @@
 using namespace std;
 
 bool *is_prime;
-int* data;
 double totaltime = 0,maxtime = 0;
 struct thread_data
 {
-    int start;
-    int end;
-    int num;
+	int start;
+	int end;
+	int num;
+	int offset;
 };
 void* primes(void *ptr)
 {
-    double begintime,endtime;
-    struct thread_data *my_data;
-    my_data = (struct thread_data*)ptr;
-    int newbegin = my_data->start + (my_data->num - my_data->start % my_data->num) % my_data->num;
-    if(newbegin > my_data->end)
+	struct thread_data *my_data;
+	my_data = (struct thread_data*)ptr;
+	int newbegin = my_data->start + (my_data->num - my_data->start % my_data->num) % my_data->num;
+	if(newbegin > my_data->end)
+		pthread_exit(NULL);
+	for(int i = newbegin;i < my_data->end;i += my_data->num)
+		if(i != my_data->num)
+			is_prime[i] = false;
 	pthread_exit(NULL);
-    for(int i = newbegin;i < my_data->end;i += my_data->num)
-	if(i != my_data->num)
-	is_prime[i] = false;
-    pthread_exit(NULL);
+}
+bool is_primef(int num)
+{
+	for(int i = 3;i * i < num;i+=2)
+		if(num % i == 0)
+			return false;
+	return true;
 }
 int main(int argc,char* argv[])
 {
-    int num = atoi(argv[1]),begin = atoi(argv[2]),end = atoi(argv[3]);
-    clock_t begintime,endtime;
-    struct thread_data td[num];
-    is_prime = new bool [end];
-    int partion_size = (int)ceil((end) * 1. / num);
-    pthread_t tids[num];
-    clock_t total_begin = clock();
+	int num = atoi(argv[1]),begin = atoi(argv[2]),end = atoi(argv[3]);
+	struct timespec begintime,endtime,total_begin,total_end;
+	struct thread_data td[num];
+	is_prime = new bool [end+1];
+	int partion_size = (int)ceil((end-begin+1) * 1. / num);
+	pthread_t tids[num];
 
-    for(int i = 0;i < end;i++)
-	is_prime[i] = true;
-
-    for(int j = 2; j * j < end;j++)
-    {
-	if(is_prime[j])
+	for(int i = 3;i <= end;i++)
 	{
-	    for(int k = j + j;k < partion_size;k+=j)
-		is_prime[k] = false;
-	    for(int i = 1;i < num;i++)
-	    {
-		td[i].num = j;
-		td[i].start = i * partion_size;
-		td[i].end = (i+1) * partion_size;
-		if(td[i].start < 2)
-		    td[i].start = 2;
-		if(td[i].end > end + 1)
-		    td[i].end = end + 1;
-
-    begintime = clock();
-		int ret = pthread_create(&tids[i],NULL,primes,(void*) &td[i]);
-    endtime = clock();
-
-    double mytime = (double)(endtime - begintime) / CLOCKS_PER_SEC;
-    if(mytime > maxtime)
-	maxtime = mytime;
-
-		if(ret)
-		{
-		    cerr << "Error create thread:" << ret << endl;
-		    exit(-1);
-		}
-	    }
-
-	    for(int l = 1;l < num;l++){
-		int rc = pthread_join(tids[l],NULL);
-		if(rc)
-		{
-		    cerr << "can't join thread:" << rc << endl;
-		    exit(-2);
-		}
-	    }
+		if(i % 2 != 0)
+			is_prime[i] = true;
+		if(i % 2 == 0)
+			is_prime[i] = false;
 	}
-    }
+	is_prime[2] = true;
+	clock_gettime(CLOCK_MONOTONIC,&total_begin);
+	for(int j = 3; j*j < end;j++)
+	{
+		if(is_prime[j])
+		{
+			for(int k = j+j;k*k < end;k+=j)
+				is_prime[k] = false;
+			for(int i = 0;i < num;i++)
+			{
+				td[i].num = j;
+				td[i].start = i * partion_size + begin;
+				td[i].end = (i+1) * partion_size + begin;
+				if(td[i].end > end + 1)
+					td[i].end = end + 1;
 
-    clock_t total_end = clock();
-    totaltime = (double)(total_end - total_begin) / CLOCKS_PER_SEC;
+				clock_gettime(CLOCK_MONOTONIC,&begintime);
+				int ret = pthread_create(&tids[i],NULL,primes,(void*) &td[i]);
+				clock_gettime(CLOCK_MONOTONIC,&endtime);
 
-    if(num == 1)
+				double mytime = endtime.tv_sec - begintime.tv_sec +
+					(endtime.tv_nsec - begintime.tv_nsec)/1000000000.0;
+				if(mytime > maxtime)
+					maxtime = mytime;
+
+				if(ret)
+				{
+					cerr << "Error create thread:" << ret << endl;
+					exit(-1);
+				}
+			}
+
+			for(int l = 1;l < num;l++){
+				int rc = pthread_join(tids[l],NULL);
+				if(rc)
+				{
+					cerr << "can't join thread:" << rc << endl;
+					exit(-2);
+				}
+			}
+			// }
+	}
+}
+
+clock_gettime(CLOCK_MONOTONIC,&total_end);
+totaltime = total_end.tv_sec - total_begin.tv_sec + (total_end.tv_nsec - total_begin.tv_nsec)/1000000000.0;
+
+if(num == 1)
 	maxtime = totaltime;
 
-    int count = 0;
-    ofstream out,time;
-    out.open("primes_p.txt",ios::out);
-    time.open("pthread_time.dat",ios::out | ios::app);
-    if(begin < 2)
-	begin = 2;
-    for(int i = begin;i < end;i++)
-    {
+	int count = 0;
+	ofstream out,time;
+	out.open("primes_p.txt",ios::out);
+	time.open("pthread_time.dat",ios::out | ios::app);
+	for(int i = begin;i <= end;i++)
+{
 	if(is_prime[i])
 	{
-	    count++;
-	    out << i << " ";
+		count++;
+		out << i<< " ";
 	}
-    }
-    time << num << " " << totaltime << " " << maxtime << endl;
+}
+//    cout << num << " " << totaltime << " " << maxtime << endl;
 
-    cout << count << endl;
+cout <<endl<< count << endl;
 
-    out.close();
-    time.close();
-    delete [] is_prime;
-    delete [] data;
-    pthread_exit(NULL);
+out.close();
+time.close();
+pthread_exit(NULL);
 }
